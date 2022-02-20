b9862467d42469019a9b664e132c6612564e7576
João Fernando <jfmoraeslara@gmail.com>
2022-02-20 11:23:26 -0300

    Método comun não estático

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -28,8 +28,8 @@ public class Product {
 		this.price = price;
 	}
 	
-	public static boolean staticProductPredicate(Product p) {
-		return p.getPrice() >= 100;
+	public boolean nonStaticProductPredicate() {
+		return price >= 100;
 	}
 	
 	@Override
