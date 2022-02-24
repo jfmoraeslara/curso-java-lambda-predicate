1d754c1b0680208cfd1681d4976f20fca877f691
João Fernando <jfmoraeslara@gmail.com>
2022-02-24 19:32:14 -0300

    Método Upper Case não estático

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 		
 		// função map é diferente da função Map - map aplica a função a cada elemento da lista
-		List<String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
+		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 		
