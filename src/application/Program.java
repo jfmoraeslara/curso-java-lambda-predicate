ef858312687847e8ebb5c7908d6d69255d279601
João Fernando <jfmoraeslara@gmail.com>
2022-02-24 19:35:05 -0300

    Upper Case lambda

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -17,8 +18,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		// função map é diferente da função Map - map aplica a função a cada elemento da lista
-		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+		
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 		
