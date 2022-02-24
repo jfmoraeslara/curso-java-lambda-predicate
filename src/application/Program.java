4b272b7be2b5c0520f8c2bbb04c3fa8583db3489
João Fernando <jfmoraeslara@gmail.com>
2022-02-24 19:29:29 -0300

    Método estático Upper Case

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseName;
 
 public class Program {
 
@@ -19,7 +18,7 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 		
 		// função map é diferente da função Map - map aplica a função a cada elemento da lista
-		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		List<String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 		
